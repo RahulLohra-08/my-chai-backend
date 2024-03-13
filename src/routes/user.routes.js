1464177881f5ea6f6ebbe986951255b7ee7ec37a
Rahul-Lohra <rahullohra13042000@gmail.com>
2024-03-13 13:41:23 +0530

    add new all routes for user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUserChannelProfile, loginUser, logoutUser, refreshAccessToken, registerUser, verifyPurchaseHistory } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, verifyPurchaseHistory } from "../controllers/user.controller.js";
 import upload from '../middleware/multer.middleware.js'
 import jwtAuth from "../middleware/auth.middleware.js";
 
@@ -20,12 +20,20 @@ router.route("/register").post(
 )
 
 router.route("/login").post(loginUser)
-router.route("/user-channel-profile/:username").get(getUserChannelProfile)
 
-router.route("/verifyPurchase").post(verifyPurchaseHistory)
 
 //secured route: user logged in or not
 router.route("/logout").post(jwtAuth, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(jwtAuth, changeCurrentPassword)
+router.route("/current-user").get(jwtAuth, getCurrentUser)
+router.route("/update-account").patch(jwtAuth, updateAccountDetails)
+// updateUserAvatar //File
+router.route("/avatar").patch(jwtAuth, upload.single("avatar"), updateUserAvatar)  //sd be login, multiple files nhi hai isliye single("avatar") ka use karenge
+router.route("/cover-image").patch(jwtAuth, upload.single("coverImage"), updateUserCoverImage)
+//params
+router.route("/user-channel-profile/:username").get(jwtAuth, getUserChannelProfile)
+router.route("/watch-history").get(jwtAuth, getWatchHistory)
+router.route("/verifyPurchase").post(verifyPurchaseHistory)
 
 export default router;
